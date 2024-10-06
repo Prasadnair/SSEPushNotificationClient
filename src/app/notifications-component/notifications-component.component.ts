d3e43c0389f7e966f211c27b3a33ea3e15533ed1
Prasadnair <prasadnair44@gmail.com>
2024-10-06 12:35:13 -0400

    Update notifications-component.component.ts

--- src/app/notifications-component/notifications-component.component.ts
+++ src/app/notifications-component/notifications-component.component.ts
@@ -1,31 +1,47 @@
 // notification.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NotificationService } from '../notification.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-notification',
   templateUrl: 'notifications-component.component.html',
   styleUrls: ['notifications-component.component.scss']
 })
-export class NotificationComponent implements OnInit {
+export class NotificationComponent implements OnInit,OnDestroy {
   public messages: string[] = [];
   private appId: string ="testing";
-  private userId: string="testing";
+  private userId: string="123";
+  subscription: Subscription | null = null;
 
-  constructor(private notificationService: NotificationService, private route: ActivatedRoute) {}
+  constructor(private notificationService: NotificationService, 
+              private route: ActivatedRoute,
+              private cd:ChangeDetectorRef) {}
 
   ngOnInit(): void {
-    // Capture route parameters
-    this.route.params.subscribe(params => {
+    // Capture route parameters   
       this.appId = "testing";// params['appId'];
       this.userId ="123";// params['userId'];
 
       // Subscribe to notifications with the appId and userId
-      this.notificationService.getNotifications(this.appId, this.userId).subscribe((message: string) => {
-        console.log(message);
-        this.messages.push(message);
+      this.subscription =this.notificationService.getNotifications(this.appId, this.userId)
+      .subscribe({
+        next: (message) => {
+          console.log('Received message:', message);
+          this.messages.push(message);
+          this.cd.detectChanges();  // Manually trigger change detection
+        },
+        error: (err) => {
+          console.error('Error receiving notifications:', err);
+        }
       });
-    });
+    
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
